cd3b8dfcf6a1b1974235a8c575430d6d893132df
Francesc Obrador <43440273+FrancescObrador@users.noreply.github.com>
2025-02-22 19:04:55 +0100

    Update Navbar.tsx

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,15 +3,21 @@ import { ThemeController } from './ThemeController'
 export const Navbar = () => {
   return (
     <div className="fixed navbar bg-base-200 text-base-content z-50">
-      <div className="flex-1">
+
+      <div className="navbar-start">
         <a className="btn btn-ghost text-xl">PeliPlan</a>
       </div>
-      <div className="flex flex-row gap-2">
+
+      <div className="navbar-center">
         <div className="form-control">
-          <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          <input type="text" placeholder="Search" className="input input-bordered w-auto md:w-128" />
         </div>
+      </div>
+
+      <div className="navbar-end">
         <ThemeController />
       </div>
+
     </div>
   )
 }
